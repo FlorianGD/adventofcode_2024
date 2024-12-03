461a51d6712fd2563812c1569ff7006b44a4f0b1
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2024-12-03 09:20:26 +0100

    day03: part1

--- src/day03.rs
+++ src/day03.rs
@@ -1,9 +1,16 @@
-pub fn parse_input(input: &str) -> Vec<u8> {
-    vec![0]
+use regex::Regex;
+
+pub fn parse_input(input: &str) -> Vec<(u32, u32)> {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    input
+        .lines()
+        .flat_map(|line| re.captures_iter(line).map(|c| c.extract()))
+        .map(|(_, [a, b])| (a.parse().unwrap(), b.parse().unwrap()))
+        .collect()
 }
 
-pub fn part1(input: Vec<u8>) -> usize {
-    0
+pub fn part1(input: Vec<(u32, u32)>) -> u32 {
+    input.iter().fold(0, |acc, (a, b)| acc + a * b)
 }
 
 #[cfg(test)]
@@ -11,13 +18,17 @@ mod tests {
     use super::*;
     use indoc::indoc;
     use pretty_assertions::assert_eq;
-
+    const INPUT: &str = indoc! {
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    };
     #[test]
     fn test_parse_input() {
-        let input = indoc! {
-            ""
-        };
-        let expected = vec![0];
-        assert_eq!(parse_input(input), expected);
+        let expected = vec![(2, 4), (5, 5), (11, 8), (8, 5)];
+        assert_eq!(parse_input(INPUT), expected);
+    }
+    #[test]
+    fn test_part1() {
+        let expected = 161;
+        assert_eq!(part1(parse_input(INPUT)), expected);
     }
 }
