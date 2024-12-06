c3e02bca93ace5f462c899e0e7362d24dceebce2
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2024-12-06 09:19:01 +0100

    day06: part 1

--- src/day06.rs
+++ src/day06.rs
@@ -1,9 +1,52 @@
-pub fn parse_input(input: &str) -> Vec<u8> {
-    vec![0]
+use num::Complex;
+use rustc_hash::FxHashMap as HashMap;
+use rustc_hash::FxHashSet as HashSet;
+
+type Pos = Complex<isize>;
+type Grid = HashMap<Pos, State>;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum State {
+    Empty,
+    Obstacle,
 }
 
-pub fn part1(input: Vec<u8>) -> usize {
-    0
+pub fn parse_input(input: &str) -> (Pos, Grid) {
+    let mut start = Complex::default();
+    let mut grid = HashMap::default();
+    for (j, line) in input.lines().enumerate() {
+        for (i, c) in line.chars().enumerate() {
+            let (pos, state) = match c {
+                '^' => {
+                    start = Complex::new(i as isize, j as isize);
+                    (Complex::new(i as isize, j as isize), State::Empty)
+                }
+                '.' => (Complex::new(i as isize, j as isize), State::Empty),
+                '#' => (Complex::new(i as isize, j as isize), State::Obstacle),
+                _ => panic!("unexpected character"),
+            };
+            grid.insert(pos, state);
+        }
+    }
+    (start, grid)
+}
+
+pub fn part1((start, grid): (Pos, Grid)) -> usize {
+    // we are facing up, but the imaginaty axis is backwards
+    let mut direction = Complex::new(0, -1);
+    let mut pos = start;
+    let mut steps = HashSet::default();
+    steps.insert(pos);
+    while let Some(state) = grid.get(&(pos + direction)) {
+        match state {
+            State::Empty => {
+                steps.insert(pos);
+                pos += direction;
+            }
+            State::Obstacle => direction *= Complex::new(0, 1),
+        }
+    }
+    steps.len() + 1
 }
 
 #[cfg(test)]
@@ -13,12 +56,31 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     const INPUT: &str = indoc! {
-        ""
+        "....#.....
+        .........#
+        ..........
+        ..#.......
+        .......#..
+        ..........
+        .#..^.....
+        ........#.
+        #.........
+        ......#...
+        "
     };
 
     #[test]
     fn test_parse_input() {
-        let expected = vec![0];
-        assert_eq!(parse_input(INPUT), expected);
+        let (start, grid) = parse_input(INPUT);
+        assert_eq!(start, Complex::new(4, 6));
+        assert_eq!(grid[&Complex::new(4, 6)], State::Empty);
+        assert_eq!(grid[&Complex::new(4, 0)], State::Obstacle);
+        assert_eq!(grid[&Complex::new(0, 0)], State::Empty);
+    }
+
+    #[test]
+    fn test_part1() {
+        let (start, grid) = parse_input(INPUT);
+        assert_eq!(part1((start, grid)), 41);
     }
 }
