514a4ad92082b3633b75db0c5ac159efc603be75
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2024-12-09 11:31:21 +0100

    day06: part 2

--- src/day06.rs
+++ src/day06.rs
@@ -50,7 +50,7 @@ pub fn part1((start, grid): (Pos, Grid)) -> usize {
     steps.len()
 }
 
-fn print_grid_and_steps(grid: &Grid, steps: &HashSet<(Pos, Dir)>) {
+fn _print_grid_and_steps(grid: &Grid, steps: &HashSet<(Pos, Dir)>) {
     let size = grid.iter().map(|(a, _)| a.re).max().unwrap();
     for j in 0..=size {
         let mut s = String::new();
@@ -79,36 +79,24 @@ fn find_loop(position: Pos, direction: Dir, steps: &HashSet<(Pos, Dir)>, grid: &
     // by 90°
     let mut grid = grid.clone();
     grid.insert(position + direction, State::Obstacle);
-    // println!("looking for a loop at {position} with {direction}");
     let mut direction = direction * Complex::i();
     let mut position = position;
     while let Some(state) = grid.get(&(position + direction)) {
-        // println!("position {position}");
         match state {
             State::Obstacle => {
-                // println!("Found an obstacle at {position}");
-                // position -= direction;
                 direction *= Complex::i();
-                // println!("new position {position} new direction {direction}");
-                // print_grid_and_steps(&grid, &steps;
             }
             State::Empty => {
                 // did we reach this point with this direction already?
                 if steps.contains(&(position, direction)) {
-                    // println!("Found a loop at {position} with {direction}");
-                    // print_grid_and_steps(&grid, &steps;
                     return true;
                 }
-                // we can continue
                 steps.insert((position, direction));
                 position += direction;
             }
         }
-        // println!("new position {position}, new direction {direction}");
     }
-    // println!("out of the grid at {position}");
-    // println!("no loop found for {position} and {direction}");
-    // print_grid_and_steps(&grid, &steps;
+    // _print_grid_and_steps(&grid, &steps;
     false
 }
 
@@ -121,75 +109,22 @@ pub fn part2((start, grid): (Pos, Grid)) -> usize {
     while let Some(state) = grid.get(&(pos + direction)) {
         match state {
             State::Empty => {
-                if find_loop(pos, direction, &steps, &grid) {
-                    blocks.insert(pos + direction);
-                }
-                pos += direction;
                 steps.insert((pos, direction));
-            }
-            State::Obstacle => direction *= Complex::i(),
-        }
-    }
-    blocks.len()
-    // not 1643
-    // 1325 too low
-    // not 2019
-    // not 1922
-}
-
-pub fn part2_old((start, grid): (Pos, Grid)) -> usize {
-    // we are facing up, but the imaginary axis is backwards
-    // println!("start {start}");
-    let mut direction = -Complex::i();
-    let mut pos = start;
-    let mut steps = HashSet::default();
-    let mut blocks: HashSet<Complex<isize>> = HashSet::default();
-    steps.insert((pos, direction));
-    // println!("Inserted {}, {}", pos, direction);
-
-    while let Some(state) = grid.get(&(pos + direction)) {
-        // println!(
-        //     "Current pos {pos}, we get state {state:?} for {}",
-        //     pos + direction
-        // );
-        match state {
-            State::Empty => {
-                // If we put a block  at pos+direction (which we can, it is empty), we
-                // will rotate 90°. Let's check if we can reach a point already on the
-                // path. If so, count it
-                let block_position = pos + direction;
-                // we cannot add a block even if the target location is empty if it is a
-                // point that we already reached. If we reach the below formation going
-                // up, we cannot add a block going down, because it would prevent us to
-                // reach this point in the first place.
-                // .#.
-                // #.#
-                // besides, we cannot add a point at the start
-                if block_position == start {
-                    println!("{block_position} was already reached with -{direction}");
+                // we cannot insert a block to a position where the guard has alreaady
+                // been or at the start.
+                if pos + direction == start || steps.iter().any(|(p, _)| *p == pos + direction) {
                     pos += direction;
-                    steps.insert((pos, direction));
                     continue;
                 }
-
                 if find_loop(pos, direction, &steps, &grid) {
                     blocks.insert(pos + direction);
                 }
-
                 pos += direction;
-                steps.insert((pos, direction));
-                // println!("Inserted {}, {}", pos, direction);
             }
             State::Obstacle => direction *= Complex::i(),
         }
     }
-    // println!("{:?}", steps);
     blocks.len()
-    // 1365 too low
-    // not 5338
-    // not 2019
-    // not 1922
-    // answer: 1796?
 }
 
 #[cfg(test)]
